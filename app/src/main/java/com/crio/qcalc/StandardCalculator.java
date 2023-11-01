fbdc7576140a46570d8e368e216857c22567e3b3
CrioUser <crio-user@criodo.com>
2023-11-01 21:24:37 +0530

    ME_QCALC_QA_MODULE_INHERITANCE

--- app/src/main/java/com/crio/qcalc/StandardCalculator.java
+++ app/src/main/java/com/crio/qcalc/StandardCalculator.java
@@ -2,61 +2,33 @@ package com.crio.qcalc;
 
 public class StandardCalculator {
 
-   private double result;
+   protected double result;
 
-   public void add(double num1, double num2) {
-       double result = num1 + num2;
-
-       if ((result == Double.MAX_VALUE) || (result == Double.POSITIVE_INFINITY)) {
-           throw new ArithmeticException("Double overflow");
-       }
-
-       this.result = result;
+   public final void add(double num1, double num2) {
+       add((double)num1, (double)num2);
    }
 
-   public void subtract(double num1, double num2) {
-       double result = num1 - num2;
-
-       if ((result == -Double.MAX_VALUE) || (result == Double.NEGATIVE_INFINITY)) {
-           throw new ArithmeticException("Double overflow");
-       }
-
-       this.result = result;
+   public final void subtract(double num1, double num2) {
+       subtract((double)num1, (double)num2);
    }
 
-   public void multiply(double num1, double num2) {
-       double result = num1 * num2;
-
-       if ((result == Double.MAX_VALUE) || (result == Double.POSITIVE_INFINITY)) {
-           throw new ArithmeticException("Double overflow");
-       }
-
-       this.result = result;
+   public final void multiply(double a, double a2) {
+       multiply((double)a, (double)a2);
    }
 
-   public void divide(double num1, double num2) {
-       if (num2 == 0.0) {
-           throw new ArithmeticException("Division by zero");
-       }
-
-       double result = num1 / num2;
-
-       if ((result == Double.MAX_VALUE) || (result == Double.POSITIVE_INFINITY)) {
-           throw new ArithmeticException("Double overflow");
-       }
-
-       this.result = result;
+   public final void divide(double num1, double num2) {
+       divide((double)num1, (double)num2);
    }
 
-   public double getResult() {
-       return result;
+   public int getResult() {
+       return (int) result;
    }
 
    public void clearResult() {
-       result = 0.0;
+       result = 0;
    }
 
    public void printResult() {
-       System.out.println("Standard Calculator Result: " + result);
+       System.out.println("Standard Calculator Result: " + (int) result);
    }
 }
