b70dd6af0b9be7ff22763aa06a29fb6f8fdd3559
CrioUser <crio-user@criodo.com>
2023-11-01 22:16:04 +0530

    ME_QCALC_QA_MODULE_INHERITANCE

--- app/src/main/java/com/crio/qcalc/StandardCalculator.java
+++ app/src/main/java/com/crio/qcalc/StandardCalculator.java
@@ -4,31 +4,59 @@ public class StandardCalculator {
 
    protected double result;
 
-   public final void add(double num1, double num2) {
-       add((double)num1, (double)num2);
+   public void add(double num1, double num2) {
+       double result = num1 + num2;
+
+       if ((result == Double.MAX_VALUE) || (result == Double.POSITIVE_INFINITY)) {
+           throw new ArithmeticException("Double overflow");
+       }
+
+       this.result = result;
    }
 
-   public final void subtract(double num1, double num2) {
-       subtract((double)num1, (double)num2);
+   public void subtract(double num1, double num2) {
+       double result = num1 - num2;
+
+       if ((result == -Double.MAX_VALUE) || (result == Double.NEGATIVE_INFINITY)) {
+           throw new ArithmeticException("Double overflow");
+       }
+
+       this.result = result;
    }
 
-   public final void multiply(double a, double a2) {
-       multiply((double)a, (double)a2);
+   public void multiply(double num1, double num2) {
+       double result = num1 * num2;
+
+       if ((result == Double.MAX_VALUE) || (result == Double.POSITIVE_INFINITY)) {
+           throw new ArithmeticException("Double overflow");
+       }
+
+       this.result = result;
    }
 
-   public final void divide(double num1, double num2) {
-       divide((double)num1, (double)num2);
+   public void divide(double num1, double num2) {
+       if (num2 == 0.0) {
+           throw new ArithmeticException("Division by zero");
+       }
+
+       double result = num1 / num2;
+
+       if ((result == Double.MAX_VALUE) || (result == Double.POSITIVE_INFINITY)) {
+           throw new ArithmeticException("Double overflow");
+       }
+
+       this.result = result;
    }
 
-   public int getResult() {
-       return (int) result;
+   public double getResult() {
+       return result;
    }
 
    public void clearResult() {
-       result = 0;
+       result = 0.0;
    }
 
    public void printResult() {
-       System.out.println("Standard Calculator Result: " + (int) result);
+       System.out.println("Standard Calculator Result: " + result);
    }
 }
