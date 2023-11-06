7bfda9c473d6c213cf24eeaa6aec6b165ebd55e0
CrioUser <crio-user@criodo.com>
2023-11-07 00:11:22 +0530

    ME_QCALC_QA_MODULE_DEBUGGING

--- app/src/test/java/com/crio/qcalc/StandardCalculatorTest.java
+++ app/src/test/java/com/crio/qcalc/StandardCalculatorTest.java
@@ -53,6 +53,21 @@ public class StandardCalculatorTest {
             }
         });
     }
+    @Test
+    @DisplayName("Test Subtraction of Two Doubles")
+    void testSubtractionOfTwoDoubles() {
+        assertThrows(ArithmeticException.class, () -> {
+            standardCalculator.subtract(-Double.MAX_VALUE, Double.MAX_VALUE);
+    });
+}
+
+    @Test
+    @DisplayName("Test Addition of Two Doubles")
+    void testAdditionOfTwoDoubles() {
+        assertThrows(ArithmeticException.class, () -> {
+            standardCalculator.add(Double.MAX_VALUE, Double.MAX_VALUE);
+        });
+    }
 
     // Other test methods for arithmetic operations can be added here
 }
